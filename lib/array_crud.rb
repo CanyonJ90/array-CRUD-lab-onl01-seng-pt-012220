23f093a9b7b08a653f3bfd93478e025bd77f7053
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-25 18:35:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,8 +31,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-   pets = ["dog", "cat", "fish", "hamster"]
-   pets[]
+   pets = ["wow", "cat", "fish", "hamster"]
+   pets[0]
 end
 
 def retrieve_last_element_from_array(array)
