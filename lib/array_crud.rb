3c551a045efe229c1f371809f0d9b23d7966d732
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-25 18:34:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,33 @@
 def create_an_empty_array
-  
+  pets = []
 end
 
 def create_an_array
-  
+  pets = ["dog", "cat", "fish", "hamster"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  pets = ["dog", "cat", "fish", "hamster"]
+  pets << "arrays!"
 end
-
-def add_element_to_start_of_array(array, element)
   
+def add_element_to_start_of_array(array, element)
+  pets = ["dog", "cat", "fish", "hamster"]
+  pets.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+ pets = ["dog", "cat", "fish", "arrays!"]
+ pets.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+   pets = ["wow", "cat", "fish", "hamster"]
+   pets.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+   pets = ["dog", "cat", "fish", "hamster"]
 end
 
 def retrieve_first_element_from_array(array)
